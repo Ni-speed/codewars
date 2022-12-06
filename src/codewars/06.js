42771e2679f0350c4d3c103ba2878fb7ef30a2d1
Ni-speed <railmir@gmail.com>
2022-12-07 04:15:13 +0500

    cw06

---
+++ src/codewars/06.js
@@ -0,0 +1,11 @@
+function invert(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== 0) {
+            array[i] = array[i] * -1;
+        } else if (array[i] == 0) {
+            array[i] = array[i] * -1
+        }
+    }
+
+    return array ;
+}
